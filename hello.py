82a516bc48dc8cbddc6512a871dd5d43772d0705
shuhei0916 <shuheiito.stc@gmail.com>
2024-06-14 16:55:17 +0900

    tested pyarmor behaviour

--- hello.py
+++ hello.py
@@ -1,23 +1,24 @@
-import pyarmor_runtime
+# import pyarmor_runtime
 import tkinter as tk
 from tkinter import messagebox
 from dist import common
 from dist import tom
+from common import *
 
-tom.hi()
+# tom.hi()
 
-# def hehe():
-#     try:
-#         check_expiration()
-#     except:
-#         messagebox.showerror("Error", error_message)
+def hehe():
+    try:
+        check_expiration()
+    except:
+        messagebox.showerror("Error", error_message)
 
-# root = tk.Tk()
-# root.title("Hello World App")
-# label = tk.Label(root, text="Hello, World!")
-# label.pack(padx=20, pady=20)
+root = tk.Tk()
+root.title("Hello World App")
+label = tk.Label(root, text="Hello, World!")
+label.pack(padx=20, pady=20)
 
-# button = tk.Button(root, text="push", command=hehe)
-# button.pack()
+button = tk.Button(root, text="push", command=hehe)
+button.pack()
 
-# root.mainloop()
+root.mainloop()
